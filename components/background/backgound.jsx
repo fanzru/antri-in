ff38b5c6a0f68ed545bdc88d0fa512a3a50dd848
Fanzru <fattahilaaf080701@gmail.com>
2021-11-15 22:53:38 +0700

    add home screen

--- components/background/backgound.jsx
+++ components/background/backgound.jsx
@@ -7,7 +7,7 @@ function Backgound(props) {
       <div className={"h-[100px] w-[100px] -right-16 top-60 z-auto rounded-full fixed bg-red-200 md:hidden"} />
       <div className={"h-[32px] w-[32px] left-80 top-16 z-auto rounded-full fixed bg-gray-400 md:hidden"} />
       <div className={"h-[150px] w-[150px] right-80 top-96 z-auto rounded-full fixed bg-gray-200 md:hidden"} />
-      <div className={"h-[150px] w-[150px] -right-20 bottom-1 z-auto rounded-full fixed bg-gray-400 md:hidden"} />
+      <div className={"h-[150px] w-[150px] -right-20 bottom-1 z-auto rounded-full fixed bg-gray-400 md:hidden"}/>
     </div>
   );
 }
