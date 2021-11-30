508e2bf5d9e54e8bb160dae21154f03db3e0fa11
Kaenova Mahendra Auditama <kaenova@gmail.com>
2021-11-30 10:34:00 +0700

    Feature: Multiple type of toast

--- pages/test-toast.js
+++ pages/test-toast.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
-import { createToast, selectToast } from '../redux/toastSlice'
+import { selectToast, createToastError, createToastSuccess, createToastWarning } from '../redux/toastSlice'
 
 function TestToast() {
   const dispatch = useDispatch(selectToast)
-  const handleClick = () => {
-    dispatch(createToast("Ini Message"))
-  }
+
   return (
-    <div className="w-scree h-screen bg-gray-400 flex justify-center items-center">
-      <button className="p-3 bg-white shadow-lg rounded-lg" onClick={handleClick} >
-        Klik untuk menampilkan Toaster
+    <div className="w-screen h-screen bg-gray-400 flex flex-col gap-5 justify-center items-center">
+      <button className="p-3 bg-white shadow-lg rounded-lg" onClick={() => {dispatch(createToastWarning("Ini Warning"))}} >
+        Klik untuk menampilkan Toaster Warning
+      </button>
+      <button className="p-3 bg-white shadow-lg rounded-lg" onClick={() => {dispatch(createToastError("Ini Error"))}} >
+        Klik untuk menampilkan Toaster Error
+      </button>
+      <button className="p-3 bg-white shadow-lg rounded-lg" onClick={() => dispatch(createToastSuccess("Ini Success"))} >
+        Klik untuk menampilkan Toaster Success
       </button>
     </div>
   )
--- redux/toastSlice.js
+++ redux/toastSlice.js
@@ -9,7 +9,7 @@ const toastSlice = createSlice({
   },
   reducers: {
     deleteToastID: (state, action) => {
-      for (let  i = 0 ; i < state.data.length ; i++ ) {
+      for (let i = 0; i < state.data.length; i++) {
         if (action.payload == state.data[i].id) {
           state.data = removeElementList(state.data, i)
           break
@@ -17,14 +17,20 @@ const toastSlice = createSlice({
       }
 
     },
-    createToast: (state, action) => {
-      state.data.push({id: nanoid(), message: action.payload})
+    createToastWarning: (state, action) => {
+      state.data.push({ id: nanoid(), message: action.payload, type: "warning" })
+    }, 
+    createToastError: (state, action) => {
+      state.data.push({ id: nanoid(), message: action.payload, type: "error" })
+    }, 
+    createToastSuccess: (state, action) => {
+      state.data.push({ id: nanoid(), message: action.payload, type: "success" })
     }
   },
 });
 
 // Export Setter
-export const { deleteToastID, createToast } = toastSlice.actions;
+export const { deleteToastID, createToastWarning, createToastError, createToastSuccess } = toastSlice.actions;
 
 // Buat store yang nge bind "nama" slice ke slice fungsinya ini dilakukan di store.js
 
