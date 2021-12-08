3c54c2c352ecdab4a1da8c9a554c3e2c22f2d633
Kaenova Mahendra Auditama <kaenova@gmail.com>
2021-12-08 18:48:06 +0700

    Adding favicon + select-none root

--- pages/_document.js
+++ pages/_document.js
@@ -11,9 +11,10 @@ class MainDocument extends Document {
       <Html>
         <Head>
           <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;" />
+          <link rel="icon" type="image/png" href="/favicon.png" />
         </Head>
         <body>
-          <div className="font_poppins">
+          <div className="font_poppins select-none">
             <div id="modal-root"></div>
             <Main />
             <NextScript />
